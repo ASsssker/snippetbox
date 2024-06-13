93b865677be9622def4950a909d69fefc959a0e6
ASsssker <ask.mol2@mail.ru>
2024-06-13 22:33:55 +0300

    add two example handlers

--- main.go
+++ main.go
@@ -6,7 +6,23 @@ import (
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
+	// If url path is not root
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
+
 	w.Write([]byte("Hello, from Snippetbox"))
+}
+
+// View snippets handler
+func snipperView(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Display a specific snippet..."))
+}
+
+// Create new snippet handler
+func snippetCreate(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Create a new snippet..."))
 }
 
 func main() {
@@ -14,6 +30,8 @@ func main() {
 	// and register Handler func.
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
+	mux.HandleFunc("/snippet/view", snipperView)
+	mux.HandleFunc("/snippet/create", snippetCreate)
 	// Use ListenAndServe func to start a new web server.
 	// We pass in tcp socket address and ServeMux router.
 	log.Println("Starting server on :4000")
