f63b365ff7411226e75670c87856b15fc156f9e8
ASsssker <ask.mol2@mail.ru>
2024-06-13 23:29:37 +0300

    refactoring project structure

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,49 +1,9 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net/http"
-	"strconv"
 )
-
-func home(w http.ResponseWriter, r *http.Request) {
-	// If url path is not root
-	if r.URL.Path != "/" {
-		http.NotFound(w, r)
-		return
-	}
-
-	w.Write([]byte("Hello, from Snippetbox"))
-}
-
-// View snippets handler
-func snipperView(w http.ResponseWriter, r *http.Request) {
-	// Extract the value of the id from the url query parameter and
-	// convert to integer.
-	id, err := strconv.Atoi(r.URL.Query().Get("id"))
-	// If an error is received during conversion or 
-	// id < 1 return not found
-	if err != nil || id < 1 {
-		http.NotFound(w, r)
-		return
-	}
-
-	fmt.Fprintf(w, "Display a specific snippet with ID %d", id)
-}
-
-// Create new snippet handler
-func snippetCreate(w http.ResponseWriter, r *http.Request) {
-	// If method is not POST
-	if r.Method != "POST" {
-		// Set header and save 
-		w.Header().Set("Allow", "POST")
-		http.Error(w, http.StatusText(http.StatusMethodNotAllowed), http.StatusMethodNotAllowed)
-		return
-	}
-
-	w.Write([]byte("Create a new snippet..."))
-}
 
 func main() {
 	// Create new ServeMux router
