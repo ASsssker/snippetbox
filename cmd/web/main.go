e747bd3168973038244b7bb4342c1803a836993a
ASsssker <ask.mol2@mail.ru>
2024-06-14 22:59:34 +0300

    added cli option to configure server address

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,11 +1,16 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
 )
 
 func main() {
+	// addr is a pointer to command line option with the nme "addr"
+	addr := flag.String("addr", ":4000", "HTTP network address")
+	// Parsing command line options
+	flag.Parse()
 	// Create new ServeMux router.
 	mux := http.NewServeMux()
 	// Create a file server which serves static files
@@ -18,7 +23,7 @@ func main() {
 	mux.HandleFunc("/snippet/create", snippetCreate)
 	// Use ListenAndServe func to start a new web server.
 	// We pass in tcp socket address and ServeMux router.
-	log.Println("Starting server on :4000")
-	err := http.ListenAndServe(":4000", mux)
+	log.Printf("Starting server on %s", *addr)
+	err := http.ListenAndServe(*addr, mux)
 	log.Fatal(err)
 }
